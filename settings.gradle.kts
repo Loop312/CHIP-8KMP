40b0aca59e6516322d1fcfba7025310ca3ae2c27
Lo_op <shaikhfarhaan312@gmail.com>
2026-09-21 18:44:36 -0400

    implement root gradle files

---
+++ settings.gradle.kts
@@ -0,0 +1,37 @@
+rootProject.name = "CHIP-8K"
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
+}
+
+include(":app-android")
+include("app-desktop")
+include("app-web")
+include(":feature-chip8")
